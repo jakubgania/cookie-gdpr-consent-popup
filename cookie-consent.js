57169e0b73b04a72e7d3a9b83c1b827961d0a5f6
jakubgania <contact@jakubgania.io>
2020-03-21 20:11:30 +0100

    update

--- cookie-consent.js
+++ cookie-consent.js
@@ -48,7 +48,7 @@ class CookieConsent {
       let data = JSON.parse(text);
       console.log(data);
       listOfVendors = data.vendors;
-      console.log(listOfVendors[0]);
+      console.log(listOfVendors[0]['name']);
     });
 
     let listData = [
