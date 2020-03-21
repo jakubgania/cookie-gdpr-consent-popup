eececf1b2acac63c01a3ef6f6600173fe462eb8d
jakubgania <contact@jakubgania.io>
2020-03-21 12:45:20 +0100

    implemented class - CookieConsent

--- cookie-consent.js
+++ cookie-consent.js
@@ -1,41 +1,46 @@
-function disableScroll() {
-  document.getElementsByTagName('html')[0].style.overflow = 'hidden'
-}
+class CookieConsent {
+  constructor() {
+    this.buttonCss = "font-size: 18px;padding-left:20px;padding-right:20px;padding-top:4px;padding-bottom:4px;border:none;outline:none;background-color: #cccccc;"
+  }
 
-function enableScroll() {
-  document.getElementsByTagName('html')[0].style.overflow = 'auto'
-}
+  disableScroll() {
+    document.getElementsByTagName('html')[0].style.overflow = 'hidden';
+  }
 
-function createWindow() {
-  cookieWindowContainer = document.createElement('div');
-  cookieWindowContainer.style.position = "absolute"
-  cookieWindowContainer.style.top = "50%";
-  cookieWindowContainer.style.left = "50%";
-  cookieWindowContainer.style.transform = "translate(-50%, -50%)";
-  cookieWindowContainer.style.width = "100%";
-  cookieWindowContainer.style.maxWidth = "600px";
-  cookieWindowContainer.style.height = "800px"
-  cookieWindowContainer.style.backgroundColor = "#f2f2f2";
-  cookieWindowContainer.setAttribute("id", "cookie-consent-window");
-  document.getElementsByTagName('body')[0].appendChild(cookieWindowContainer);
-}
+  enableScroll() {
+    document.getElementsByTagName('html')[0].style.overflow = 'auto';
+  }
 
-function createHeader() {
-  header = document.createElement('div');
-  header.style.textAlign = "center";
-  header.style.fontSize = "20px";
-  header.style.borderBottom = "1px solid #b3b3b3";
-  header.setAttribute("id", "header");
-  document.getElementById('cookie-consent-window').appendChild(header);
-  para = document.createElement("p");
-  node = document.createTextNode("GDPR consent");
-  para.appendChild(node);
-  element = document.getElementById("header");
-  element.appendChild(para);
-}
+  createWindow() {
+    let cookieWindowContainer = document.createElement('div');
+    cookieWindowContainer.style.position = "absolute"
+    cookieWindowContainer.style.top = "50%";
+    cookieWindowContainer.style.left = "50%";
+    cookieWindowContainer.style.transform = "translate(-50%, -50%)";
+    cookieWindowContainer.style.width = "100%";
+    cookieWindowContainer.style.maxWidth = "600px";
+    cookieWindowContainer.style.height = "800px"
+    cookieWindowContainer.style.backgroundColor = "#f2f2f2";
+    cookieWindowContainer.setAttribute("id", "cookie-consent-window");
+    document.getElementsByTagName('body')[0].appendChild(cookieWindowContainer);
+  }
 
-function createList() {
-  let listData = [
+  createHeader() {
+    let header = document.createElement('div');
+    header.style.textAlign = "center";
+    header.style.fontSize = "20px";
+    header.style.borderBottom = "1px solid #b3b3b3";
+    header.setAttribute("id", "header");
+    document.getElementById('cookie-consent-window').appendChild(header);
+    let para = document.createElement("p");
+    let node = document.createTextNode("GDPR consent");
+    para.appendChild(node);
+    let element = document.getElementById("header");
+    element.appendChild(para);
+  }
+
+  createList() {
+    let listData = [
       'example 1',
       'example 2',
       'example 3',
@@ -43,91 +48,97 @@ function createList() {
       'example 5',
       'example 6',
       'example 7'
-  ],
+    ],
 
-  listContainer = document.createElement('div'),
- 
-  listElement = document.createElement('ul'),
+    listContainer = document.createElement('div'),
+  
+    listElement = document.createElement('ul'),
 
-  numberOfListItems = listData.length,
-  listItem,
-  i;
+    numberOfListItems = listData.length,
+    listItem,
+    i;
 
-  document.getElementById('cookie-consent-window').appendChild(listContainer);
-  listContainer.appendChild(listElement);
+    document.getElementById('cookie-consent-window').appendChild(listContainer);
+    listContainer.appendChild(listElement);
 
-  for (i = 0; i < numberOfListItems; ++i) {
-      listItem = document.createElement('li');
-      listItem.innerHTML = listData[i];
-      listElement.appendChild(listItem);
+    for (i = 0; i < numberOfListItems; ++i) {
+        listItem = document.createElement('li');
+        listItem.innerHTML = listData[i];
+        listElement.appendChild(listItem);
+    }
+  }
+
+  createRejectButton() {
+    let elementRejectButtonSection = document.getElementById("reject-section");
+    elementRejectButtonSection.style.cssText = "width: 50%;text-align: center;";
+    let btn = document.createElement('button');
+    btn.style.cssText = this.buttonCss;
+    btn.innerHTML = 'Reject';
+    btn.onclick = function(){
+      this.enableScroll();
+      this.closeWindow();
+      return false;
+    };
+    elementRejectButtonSection.appendChild(btn);
+  }
+
+  createAcceptButton() {
+    let elementAcceptButtonSection = document.getElementById("accept-button");
+    elementAcceptButtonSection.style.cssText = "width: 50%;text-align: center;";
+    let button = document.createElement('button');
+    button.style.cssText = this.buttonCss;
+    button.innerHTML = 'Accept';
+    button.onclick = () => {
+      // this.enableScroll();
+      this.closeWindow();
+      return false;
+    };
+    elementAcceptButtonSection.appendChild(button);
+  }
+
+  createFooter() {
+    let footer = document.createElement('div');
+    footer.style.borderTop = "1px solid #b3b3b3";
+    footer.style.display = "flex";
+    footer.style.height = "60px";
+    footer.style.lineHeight = "60px";
+    footer.setAttribute("id", "footer");
+    document.getElementById('cookie-consent-window').appendChild(footer);
+    let element = document.getElementById("footer");
+    let rejectSection = document.createElement('div');
+    rejectSection.setAttribute("id", "reject-section");
+
+    let acceptSection = document.createElement('div');
+    acceptSection.setAttribute("id", "accept-button");
+    element.appendChild(rejectSection);
+    element.appendChild(acceptSection);
+
+    this.createRejectButton();
+    this.createAcceptButton();
+  }
+
+  closeWindow() {
+    let element = document.getElementById("cookie-consent-window");
+    element.style.display = "none";
+  }
+
+  // check cookies
+  // check protocol type
+
+  render() {
+    let stateCheck = setInterval(() => {
+      if (document.readyState === 'complete') {
+        clearInterval(stateCheck);
+        
+        this.disableScroll();
+        this.createWindow();
+        this.createHeader();
+        this.createList();
+        this.createFooter();
+      }
+    }, 100);
   }
 }
 
-function createRejectButton() {
-  elementRejectButtonSection = document.getElementById("reject-section");
-  elementRejectButtonSection.style.cssText = "width: 50%;text-align: center;";
-  btn = document.createElement('button');
-  btn.innerHTML = 'Reject';
-  btn.onclick = function(){
-    enableScroll();
-    closeWindow();
-    return false;
-  };
-  elementRejectButtonSection.appendChild(btn);
-}
-
-function createAcceptButton() {
-  elementAcceptButtonSection = document.getElementById("accept-button");
-  elementAcceptButtonSection.style.cssText = "width: 50%;text-align: center;";
-  button = document.createElement('button');
-  button.style.cssText = "font-size: 18px;padding-left:20px;padding-right:20px;padding-top:4px;padding-bottom:4px;border:none;outline:none;background-color: #cccccc;";
-  button.innerHTML = 'Accept';
-  button.onclick = function(){
-    enableScroll();
-    closeWindow();
-    return false;
-  };
-  elementAcceptButtonSection.appendChild(button);
-}
-
-function createFooter() {
-  footer = document.createElement('div');
-  footer.style.borderTop = "1px solid #b3b3b3";
-  footer.style.display = "flex";
-  footer.style.height = "60px";
-  footer.style.lineHeight = "60px";
-  footer.setAttribute("id", "footer");
-  document.getElementById('cookie-consent-window').appendChild(footer);
-  element = document.getElementById("footer");
-  rejectSection = document.createElement('div');
-  rejectSection.setAttribute("id", "reject-section");
-
-  acceptSection = document.createElement('div');
-  acceptSection.setAttribute("id", "accept-button");
-  element.appendChild(rejectSection);
-  element.appendChild(acceptSection);
-
-  createRejectButton();
-  createAcceptButton();
-}
-
-function closeWindow() {
-  element = document.getElementById("cookie-consent-window");
-  element.style.display = "none";
-}
-
-// check cookies
-// check protocol type
-
-let stateCheck = setInterval(() => {
-  if (document.readyState === 'complete') {
-    clearInterval(stateCheck);
-    
-    disableScroll();
-    createWindow();
-    createHeader();
-    createList();
-    createFooter();
-  }
-}, 100);
-
+const cookie = new CookieConsent();
+cookie.render();
